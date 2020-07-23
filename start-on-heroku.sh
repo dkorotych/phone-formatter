51f5dbee082c3278ccb67fe3ef73ec7fe1011540
Dmitry Korotych <dkorotych@gmail.com>
2020-07-23 16:38:22 +0300

    Add specific settings for run on Heroku

---
+++ start-on-heroku.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+java "$JAVA_OPTS" -jar build/libs/phone-formatter-0.1-all.jar
+for file in hs_err_pid*.log; do
+  if [ -f "$file" ]; then
+    cat hs_err_pid*.log
+    break
+  fi
+done
