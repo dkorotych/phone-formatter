3fd51a905940bbd99fc9958ff7e39a134af0737c
Dmitry Korotych <dkorotych@gmail.com>
2021-09-15 16:47:50 +0300

    Fix accessible exceptions in emoji4j

--- start-on-heroku.sh
+++ start-on-heroku.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -jar build/libs/phone-formatter-0.1-all.jar
+java --add-opens java.base/java.lang=ALL-UNNAMED -jar build/libs/phone-formatter-0.1-all.jar
 for file in hs_err_pid*.log; do
   if [ -f "$file" ]; then
     cat hs_err_pid*.log
