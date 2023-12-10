d06af9a69e148949d09df5ddef3381ce6a6a1672
Dmitry Korotych <dkorotch@gmail.com>
2023-12-10 22:45:36 +0300

    Java 21 support Emoji

--- src/main/java/com/github/dkorotych/phone/region/FlagsKeeper.java
+++ src/main/java/com/github/dkorotych/phone/region/FlagsKeeper.java
@@ -2,17 +2,13 @@ package com.github.dkorotych.phone.region;
 
 import com.github.dkorotych.phone.region.domain.Flag;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
-import com.vdurmont.emoji.Emoji;
-import com.vdurmont.emoji.EmojiManager;
-import io.micronaut.core.util.StringUtils;
 import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
@@ -22,28 +18,18 @@ public class FlagsKeeper extends AbstractRegionMapper<Flag> {
     @Override
     protected Supplier<Map<String, Flag>> getSupplier() {
         return () -> {
-            final Map<String, Emoji> flags = EmojiManager.getForTag("flag").stream().
-                    filter(emoji -> emoji.getAliases().stream().
-                            findFirst().
-                            map(StringUtils::hasText).
-                            isPresent()).
-                    collect(Collectors.toMap(emoji -> {
-                        String region = emoji.getAliases().getFirst().toUpperCase(Locale.ENGLISH);
-                        if (region.endsWith("_FLAG")) {
-                            region = region.substring(0, region.indexOf("_FLAG"));
-                        }
-                        return region;
-                    }, Function.identity()));
             final Set<String> regions = PhoneNumberUtil.getInstance().getSupportedRegions();
             final Map<String, Flag> map = new HashMap<>(regions.size());
             for (final String region : regions) {
-                final Emoji emoji = flags.get(region);
-                if (emoji != null) {
-                    final String code = emoji.getUnicode();
-                    final String htmlCode = emoji.getHtmlDecimal();
-                    final Flag flag = new Flag(code, htmlCode);
-                    map.put(region, flag);
-                }
+                final int[] codePoints = region.codePoints().
+                        map(operand -> 127397 + operand).
+                        toArray();
+                final String code = new String(codePoints, 0, codePoints.length);
+                final String htmlCode = Arrays.stream(codePoints).
+                        mapToObj("&#%d;"::formatted).
+                        collect(Collectors.joining());
+                final Flag flag = new Flag(code, htmlCode);
+                map.put(region, flag);
             }
             return map;
         };
