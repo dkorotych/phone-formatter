2f944399d3ab121e69453645419d95b49c096130
Dmitry Korotych <dkorotych@gmail.com>
2024-02-24 20:38:37 +0300

    migrate to Micronaut 4.3.3

--- src/main/java/com/github/dkorotych/phone/micronaut/security/login/PredefinedUserAuthenticationProvider.java
+++ src/main/java/com/github/dkorotych/phone/micronaut/security/login/PredefinedUserAuthenticationProvider.java
@@ -1,34 +1,28 @@
 package com.github.dkorotych.phone.micronaut.security.login;
 
 import com.github.dkorotych.phone.micronaut.configuration.User;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.security.authentication.AuthenticationFailureReason;
-import io.micronaut.security.authentication.AuthenticationProvider;
 import io.micronaut.security.authentication.AuthenticationRequest;
 import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
-import org.reactivestreams.Publisher;
 
 import java.util.Objects;
 
 @RequiredArgsConstructor
-public abstract class PredefinedUserAuthenticationProvider implements AuthenticationProvider<HttpRequest<?>> {
+public abstract class PredefinedUserAuthenticationProvider implements HttpRequestAuthenticationProvider<Object> {
     private final User user;
 
-    @Override
-    public Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        return emitter -> {
-            if (Objects.equals(user.getIdentity(), authenticationRequest.getIdentity())
-                    && Objects.equals(user.getSecret(), authenticationRequest.getSecret())) {
-                emitter.onNext(create());
-            } else {
-                emitter.onError(AuthenticationResponse.exception(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH));
-            }
-        };
-    }
-
-    protected AuthenticationResponse create() {
-        return AuthenticationResponse.success(user.getIdentity(), user.getRoles());
+    public AuthenticationResponse authenticate(@Nullable HttpRequest<Object> httpRequest,
+                                               @NonNull AuthenticationRequest<String, String> authenticationRequest) {
+        if (Objects.equals(user.getIdentity(), authenticationRequest.getIdentity())
+                && Objects.equals(user.getSecret(), authenticationRequest.getSecret())) {
+            return AuthenticationResponse.success(user.getIdentity(), user.getRoles());
+        } else {
+            return AuthenticationResponse.failure(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH);
+        }
     }
 }
